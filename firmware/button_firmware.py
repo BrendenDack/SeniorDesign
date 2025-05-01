725d8f2ca664099416750bf138a5c621f4d99724
BrendenDack <brendendack@csu.fullerton.edu>
2025-04-30 19:32:41 -0700

    Fixed accidental capitalization of function name

--- firmware/button_firmware.py
+++ firmware/button_firmware.py
@@ -4,7 +4,7 @@ import sys
 import time
 import logging
 import threading
-from stt import Start_voice_recognition
+from stt import start_voice_recognition
 try:
     import RPi.GPIO as GPIO
 except ModuleNotFoundError:
@@ -72,7 +72,7 @@ class ButtonFirmware:
         elif channel == BACK_BUTTON:
             logging.info("Back Button (GPIO %d) pressed - Triggering Voice Recognition", BACK_BUTTON)
             #Run the voice recognition in a seperate thread
-            recognition_thread = threading.Thread(target=Start_voice_recognition)
+            recognition_thread = threading.Thread(target=start_voice_recognition)
             recognition_thread.daemon=True # Daemonize the thread to allow it to exit with the main program
             recognition_thread.start() # Start the recognition process
         elif channel == RIGHT_BUTTON:
